ccf15407c1168e40f7534e299db030227651a357
Nícolas Iensen <nicolas@iensen.me>
2013-05-31 18:39:25 -0300

    Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gota::Application.config.secret_token = '003ae962765b508306ff347f3ac1ef773fa217f20290ced869296676e07fe8586385a62e58e27806b3181d5af463de098bdfc9e2c1cec3857983624f94c9ca25'
